048fdd743e7eb0f6d5259b96c204e151a59cc6c3
kevin <kvn.wernet@gmail.com>
2020-05-25 23:39:38 -0600

    Add comment + clean the code

--- js/main.js
+++ js/main.js
@@ -7,31 +7,32 @@ function getInfo(){
     callLogin(username,passhash);
 }
 
+// store data with localStorage provides
 function storeData(data) {
     for (key in data) {
         localStorage.setItem(key, data[key]);
     }
 }
 
-
-
+//First call to log in user
 function callLogin(username,password) {
     const Url = 'https://crtoea7d0a.execute-api.us-east-1.amazonaws.com/users/login?';
     let params = {
         u: username,
         hp: password
     };
-
+    //Add the param to the url
     let query = Object.keys(params)
         .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
         .join('&');
 
     let url = Url + query;
-
+    //make the call
     fetch(url).then(function (response) {
         let statusCode = response.status;
         response.text().then(function(text) {
                 let obj = JSON.parse(text);
+                //check the status of the call
                 if (statusCode === 400 || statusCode === 401)
                 {
                     document.getElementById("errorMessage").innerHTML = obj.error;
@@ -39,7 +40,6 @@ function callLogin(username,password) {
                 }
                 else{
                     storeData(obj);
-                   // const dataToken = getData();
                     document.getElementById("login").style.display = "none";
                     document.getElementById("logged").style.display = "block";
                     document.getElementById("label").innerHTML = obj.seetrusLabel;
@@ -47,7 +47,7 @@ function callLogin(username,password) {
                 }
 
             }).catch(function (error) {
-            console.log('request failed', error)
+                //alert the user the call has failed
             document.getElementById("errorMessage").innerHTML = "Please retry again.";
             document.getElementById("errorMessage").style.display = "block";
         })
@@ -61,18 +61,20 @@ function callNext() {
     let url = Url + groupId + "/next";
     const myHeaders = new Headers();
     myHeaders.append("SeetrusAuthenticationToken", "uu58354A40FCB94759AC8FE0148A8DF116544ADBEF503441B5891914F6677C844A");
-
+    //create the header
     const requestOptions = {
         method: 'GET',
         headers: myHeaders,
         redirect: 'follow'
     };
-
+    //make the second call
     fetch(url, requestOptions)
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
 }
+
+//check if the token is already set and if the user has to log in
 function checkUser(){
     if ( localStorage.getItem('token')!= null ) {
         document.getElementById("logged").style.display = "block";
